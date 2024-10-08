8f42cacbc30c155d2eabb8c20ec36370ab7b3f96
marboccu <martina.boccuzzi@sourcesense.com>
2024-10-08 14:09:10 +0200

    add info scritp

---
+++ info.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# this script displays path to homedir, the terminal type and list of running services
+
+clear
+
+echo "This script will give us environment information"
+echo "================================================"
+echo
+
+echo "This is homedir: $HOME"
+echo
+
+echo "This is terminal type: $TERM"
+echo
+
+echo "This is list of services: "
+launchctl list
+echo

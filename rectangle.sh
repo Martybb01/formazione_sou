1d6f7b16df8ff7149c171ae5de661ab2c4a068bd
marboccu <martina.boccuzzi@sourcesense.com>
2024-10-08 14:43:19 +0200

    fix script

--- rectangle.sh
+++ rectangle.sh
@@ -3,9 +3,16 @@
 
 clear
 
-height=10
-width=20
-area=$((height * width))
+height=$1
+width=$2
+
+if [ -z "$height" ] || [ -z "$width" ]; then
+  echo "Please provide the height and width of the rectangle"
+  echo "Usage: $0 <height> <width>"
+  exit 1
+fi
+
+area=$((($height * $width)/2))
 
 echo "The area of the rectangle is $area"
 echo

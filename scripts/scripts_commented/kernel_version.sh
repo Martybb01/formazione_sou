a0f7b628d49ef47698172d077d22f36901812274
marboccu <martina.boccuzzi@sourcesense.com>
2024-10-21 16:42:35 +0200

    check kernel version for RHEL

---
+++ scripts/scripts_commented/kernel_version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if the kernel version running is the latest one
+
+current_kernel=$(uname -r)
+
+latest_kernel=$(rpm -q kernel | sed 's/kernel-//g' | tail -n 1)
+
+if [ "$current_kernel" == "$latest_kernel" ]; then
+	echo "TRUE"
+else
+	echo "FALSE"
+fi
+
+exit 0
+
+#------------------------------------------
+# Other possibilities for finding the running kernel version
+
+# current_kernel=$(hostnamectl | grep "Kernel" | awk '{print $3}')
+# 
+

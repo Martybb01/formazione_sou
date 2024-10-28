9abc8c116385fa039e192c0bf3cf014323f885f3
marboccu <martina.boccuzzi@sourcesense.com>
2024-10-28 15:29:57 +0100

    better port validation

--- scripts/port_scanner.sh
+++ scripts/port_scanner.sh
@@ -8,18 +8,24 @@ fi
 IP=$1
 PORT_RANGE=$2
 
-if ! echo $IP | grep -qE "^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|[1-9]?[0-9])$"; then
+if ! echo "$IP" | grep -qE "^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|[1-9]?[0-9])$"; then
 	echo "Invalid IP address"
-	exit 1
-fi
-
-if ! echo $PORT_RANGE | grep -qE "^(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{0,3})(-(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{0,3}))?$"; then
-	echo "Invalid port range"
 	exit 1
 fi
 
 START_PORT=$(echo $PORT_RANGE | cut -d'-' -f1)
 END_PORT=$(echo $PORT_RANGE | cut -d'-' -f2)
+
+check_port() {
+    local port=$1
+    if ! [[ $port =~ ^[0-9]+$ ]] || ((port <= 0 || port > 65535)); then
+        echo "Port $port is out of range (1-65535)"
+        exit 1
+    fi
+}
+
+check_port $START_PORT
+check_port $END_PORT
 
 if [ $START_PORT -gt $END_PORT ]; then
 	TEMP=$START_PORT
@@ -48,3 +54,5 @@ exit 0
 # Il risultato di nc viene memorizzato nella variabile result, così da poter controllare se la porta è "open".
 # awk viene utilizzato per estrarre il nome del servizio dall'output di nc, dividendo la riga in base ai caratteri '()'.
 # Se la porta è aperta, viene stampato un messaggio con il numero di porta e il servizio corrispondente.
+
+# Regex x validazione porte --> "^(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{0,3})(-(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{0,3}))?$"

be7a2295ab41e9a05a84bfbc458d57a914955bc5
marboccu <martina.boccuzzi@sourcesense.com>
2024-11-18 19:35:36 +0100

    fix script

--- formazione_sou_k8s/export-deployment.sh
+++ formazione_sou_k8s/export-deployment.sh
@@ -3,10 +3,10 @@
 echo "Exporting deployment..."
 TOKEN=$(kubectl -n formazione-sou get secret cluster-reader-token -o jsonpath="{.data.token}" | base64 --decode)
 API_SERVER=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
+CA_CERT=$(kubectl config view --raw -o jsonpath='{.clusters[0].cluster.certificate-authority-data}' | base64 --decode > /home/vagrant/ca.crt)
+CERT_FILE="/home/vagrant/ca.crt"
 NAMESPACE="formazione-sou"
 DEPLOYMENT="flask-app"
 OUTFILE="/home/vagrant/deployment.yaml"
 
-kubectl get deployment $DEPLOYMENT -n $NAMESPACE -o yaml --token=$TOKEN --server=$API_SERVER > $OUTFILE
-
-echo "Deployment exported to $OUTFILE"
+kubectl --kubeconfig=/dev/null --token=$TOKEN --server=$API_SERVER --certificate-authority=$CERT_FILE -n $NAMESPACE get deployment $DEPLOYMENT -o yaml > $OUTFILE

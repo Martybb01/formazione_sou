6be2092bc03e66aa58278149240eeeb8d6cbeffc
marboccu <martina.boccuzzi@sourcesense.com>
2024-11-28 17:36:20 +0100

    encrypt private key for ssh access + script to encrypt/decrypt and connect

---
+++ formazione_cm/ssh_access.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONTAINER_PORT_ALPINE=4200
+CONTAINER_PORT_ROCKY=4201
+
+# Decrypt file key
+ansible-vault decrypt ./id_key_genuser --vault-password-file ./vault_pwd
+
+# SSH to remote server
+ssh -i ./id_key_genuser -p $CONTAINER_PORT_ROCKY genuser@localhost
+
+# Encrypt file key again
+ansible-vault encrypt ./id_key_genuser --vault-password-file ./vault_pwd

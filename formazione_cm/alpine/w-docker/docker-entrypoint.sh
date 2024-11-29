c1cec94d191f840fe229659d9f315cd5c1d08f14
marboccu <martina.boccuzzi@sourcesense.com>
2024-11-29 15:38:05 +0100

    add alpine container w docker service setup w ansible task to run it

---
+++ formazione_cm/alpine/w-docker/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+# Regenerate ssh key
+ssh-keygen -A
+
+# Create the directories
+if [ ! -d "/var/run/sshd" ]; then
+    mkdir -p /var/run/sshd
+fi
+
+if [ ! -d "/root/.ssh" ]; then
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+fi
+
+# Start the SSH service
+/usr/sbin/sshd -D &
+# Start the Docker service
+echo "${AUTHORIZED_KEYS:-}" > /root/.ssh/authorized_keys
+mkdir -p /root/.docker/ && echo ${DOCKER_CONFIG_JSON:-} > /root/.docker/config.json
+exec dockerd
--- formazione_cm/ssh_access.sh
+++ formazione_cm/ssh_access.sh
@@ -2,12 +2,13 @@
 
 CONTAINER_PORT_ALPINE=4200
 CONTAINER_PORT_ROCKY=4201
+CONTAINER_PORT_DOCKER=4202
 
 # Decrypt file key
 ansible-vault decrypt ./id_key_genuser --vault-password-file ./vault_pwd
 
 # SSH to remote server
-ssh -i ./id_key_genuser -p $CONTAINER_PORT_ROCKY genuser@localhost
+ssh -i ./id_key_genuser -p $CONTAINER_PORT_DOCKER genuser@localhost
 
 # Encrypt file key again
 ansible-vault encrypt ./id_key_genuser --vault-password-file ./vault_pwd

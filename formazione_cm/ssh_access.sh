f4b4a4cd6959a6a00d0b6500e98290a1c213a789
marboccu <martina.boccuzzi@sourcesense.com>
2024-12-02 18:07:08 +0100

    modify script

--- formazione_cm/ssh_access.sh
+++ formazione_cm/ssh_access.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-CONTAINER_PORT_ALPINE=4200
-CONTAINER_PORT_ROCKY=4201
 CONTAINER_PORT_DOCKER=4202
 
 # Decrypt file key
 ansible-vault decrypt ./id_key_genuser --vault-password-file ./vault_pwd
 
 # SSH to remote server
-ssh -i ./id_key_genuser -p $CONTAINER_PORT_DOCKER genuser@localhost
+ssh -i ./id_key_genuser -p $CONTAINER_PORT_DOCKER genuser@127.0.0.1
+# ansible-playbook -i inventory deploy_containers.yml --vault-password-file ./vault_pwd -vvv
 
 # Encrypt file key again
 ansible-vault encrypt ./id_key_genuser --vault-password-file ./vault_pwd
